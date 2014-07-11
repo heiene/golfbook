2e0f7fc83879e49366d149e70bcdc5c405ce9894
Øyvind Heiene <oyvindheiene@hotmail.com>
2014-07-11 22:09:00 +0200

    skiftet til foundation

--- public/js/app.js
+++ public/js/app.js
@@ -7,6 +7,7 @@ angular.module('golfApp',
         'CourseCtrl',
         'RoundCtrl',
         'appRoutes',
+        'mm.foundation',
         'UserService',
 		'CourseService']
 	);
--- server.js
+++ server.js
@@ -18,7 +18,7 @@ var cookieParser 	= require('cookie-parser')
 var db = require('./config/db');
 
 var port = process.env.PORT || 8080; // set our port
-mongoose.connect(db.local); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
+mongoose.connect(db.url); // connect to our mongoDB database (uncomment after you enter in your own credentials in config/db.js)
 
 app.use(express.static(__dirname + '/public')); 	// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); 					// log every request to the console
