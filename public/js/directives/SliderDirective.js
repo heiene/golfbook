5bfb2ed613384816590ba11a1f60b347bee8a050
oyvind <oyvindheiene@hotmail.com>
2014-08-05 21:28:42 +0200

    fiksa slider directive

--- public/js/directives/SliderDirective.js
+++ public/js/directives/SliderDirective.js
@@ -15,25 +15,32 @@ angular.module('SliderDirective', [])
             templateUrl: "../../templates/scoreslider_vertical_temp.html",
 //            template: 'Sliderinput: <input class="test2" ng-model="slidermodel"/>' +'{{sliderid}}<div id="{{sliderid}}"></div>', //må bare få denne til å oppdater slider så er det i boks...
             link: function(scope, elem, attrs) {
+                var slida = $("#"+elem.attr("id")+' .slida');
+                var slidaInput = $("#"+elem.attr("id")+' .slida-input');
 
-                console.log(scope.sliderid, $('#'+scope.sliderid))
-                scope.$apply();
-                console.log(scope.sliderid, $('#'+scope.sliderid))
+                console.log("elem.id",elem.attr("id"));
 
-console.log(scope.par);
+                console.log("div div div", $("#"+elem.attr("id")+' .slida'));
+
+//                console.log(scope.sliderid, $('#'+scope.sliderid))
+////                scope.$apply();
+//                console.log(scope.sliderid, $('#'+scope.sliderid))
+
+//console.log(scope.par);
 
                 var options = {
-                    start: 3,
+                    start: [scope.par],
                     range: {
                         'min': 1,
                         'max': ((scope.par*2)+1)
                     },
                     orientation: "horizontal",
                     step: 1,
+                    connect: "lower",
                     serialization: {
                     lower: [
                         $.Link({
-                            target: $("#"+scope.sliderinputid),
+                            target: slidaInput,
                             format: {
                                 decimals: 0
                             }
@@ -42,21 +49,30 @@ console.log(scope.par);
                     }
                 };
 
-                $('#'+scope.sliderid).noUiSlider(options)
+//                elem.noUiSlider(options);
+//                elem.on('set', function(){
+//                    scope.$apply(function() {
+//                        scope.slidermodel = elem.val();
+//                    })
+//                });
 
-                $("#"+scope.sliderinputid).on({
+
+
+                slida.noUiSlider(options)
+
+                slida.on({
                     set: function(n,a) {
-                            console.log(n,a,'SET IS CALLED, value: ',  $("#"+scope.sliderinputid).val())
+                            console.log(n,a,'SET IS CALLED, value: ',  slida.val())
                         scope.$apply(function () {
-                            scope.slidermodel = $("#"+scope.sliderinputid).val();
+                            scope.slidermodel = slida.val();
                         })
                     }
                     ,
                     slide: function() {
-                        console.log('SLIDE IS CALLED, value: ',  $("#"+scope.sliderinputid).val())
+                        console.log('SLIDE IS CALLED, value: ',  slida.val())
 
                         scope.$apply(function () {
-                            scope.slidermodel = $("#"+scope.sliderinputid).val();
+                            scope.slidermodel = slida.val();
                         })
                     }
                 });
